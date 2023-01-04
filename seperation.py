d64a257b46e6dceff37e5b1825c7d219047f27d4
DevyaniShekar <121898515+DevyaniShekar@users.noreply.github.com>
2023-01-04 16:26:53 +0000

    Delete seperation.py

--- seperation.py
+++
@@ -1,8 +0,0 @@
-#Write a program called seperation.py that inputs a sentence and then displays each word of the sentence
-# on
-# a
-# separate 
-# line
-sentence = input("Enter a sentence:")
-for item in sentence.split(" "):
-    print(item)
