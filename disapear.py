27fb937acc82b4bc1ca011551d06d23cea8e6a9b
DevyaniShekar <121898515+DevyaniShekar@users.noreply.github.com>
2023-01-06 09:53:55 +0000

    Delete disapear.py

--- disapear.py
+++
@@ -1,9 +0,0 @@
-#Write a Python program called disappear.py that strips a set of characters from a string.
-#Ask the user to input a string and then ask the user which characters they would like to make disappear.
-phrase = input("Enter a string: ")
-print(phrase)
-
-characters = input("Enter the characters you would like to make disappear: ")
-print(characters)
-
-print(phrase.replace(characters," "))
