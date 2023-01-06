d6564f3501a8a2aaf9e34289b8ebcfdde9170a6e
DevyaniShekar <121898515+DevyaniShekar@users.noreply.github.com>
2023-01-06 09:52:52 +0000

    Delete replace.py

--- replace.py
+++
@@ -1,6 +0,0 @@
-string = "The!quick!brown!fox!jumps!over!the!lazy!dog."
-string = string.replace("!"," ")
-print(string.replace("!"," "))
-print(string.upper())
-position = string[::-1]
-print(position)
